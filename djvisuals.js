0d4397bd115229fbb18ffb5a00e9c8dbd0208d26
JackPadalino <pada0867@gmail.com>
2023-10-31 16:10:48 -0400

    have text color changing over black background

---
+++ djvisuals.js
@@ -0,0 +1,12 @@
+// // Get the text color of the first .djText element
+// const textColor = getComputedStyle(document.querySelector(".djText")).color;
+
+// // Calculate the inverse color
+// function invertColor(hex) {
+//   return (
+//     "#" + (0xffffff ^ parseInt(hex.slice(1), 16)).toString(16).padStart(6, "0")
+//   );
+// }
+
+// // Set the inverse color as the body background color
+// document.body.style.backgroundColor = invertColor(textColor);
